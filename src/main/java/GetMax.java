bd73011bf3e3c7a13b3295cf3629165d189c27c4
Khup Taithul <aatuang@gmail.com>
2023-02-19 02:41:57 +0000

    fixed the max method

--- src/main/java/GetMax.java
+++ src/main/java/GetMax.java
@@ -7,6 +7,12 @@ public class GetMax {
      * @return the largest value in arr.
      */
     public int max(int[] arr){
-        return -1;
+        int maxNum = arr[0];
+        for(int a: arr){
+            if(a>maxNum){
+                maxNum = a;
+            }
+        }
+        return maxNum;
     }
 }
